cce682f0281b990db2f0e9fb56e3344c2f978004
KellyReisLee <kristinaalvares28@gmail.com>
2024-05-13 12:24:59 +0100

    creating the mealItem, meals components and a database with better-sqlite3

---
+++ initdb.js
@@ -0,0 +1,43 @@
+const sql = require('better-sqlite3');
+const db = sql('meals.db');
+const dummyMeals = require('./dummy_data')
+
+
+// Prepare a estrutura do banco de dados onde a tabela recebe o nome de meals.
+// Each item in this table is a column.
+db.prepare(`
+   CREATE TABLE IF NOT EXISTS meals (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       slug TEXT NOT NULL UNIQUE,
+       title TEXT NOT NULL,
+       image TEXT NOT NULL,
+       summary TEXT NOT NULL,
+       instructions TEXT NOT NULL,
+       creator TEXT NOT NULL,
+       creator_email TEXT NOT NULL
+    )
+`).run();
+
+
+
+// 
+async function initData() {
+  const stmt = db.prepare(`
+      INSERT INTO meals VALUES (
+         null,
+         @slug,
+         @title,
+         @image,
+         @summary,
+         @instructions,
+         @creator,
+         @creator_email
+      )
+   `);
+
+  for (const meal of dummyMeals) {
+    stmt.run(meal);
+  }
+}
+
+initData();
